7ded6ef1131e26969cdc616168e2f5e9f8763664
Matthieu Riolo <matthieu.riolo@googlemail.com>
2016-08-18 20:00:41 +0200

    correcting rbac dbmanager

--- modules/cii/vendor/cii/rbac/DbManager.php
+++ modules/cii/vendor/cii/rbac/DbManager.php
@@ -1,10 +1,16 @@
 <?php
 
-namespace yii\filters;
+namespace cii\rbac;
 
 use app\modules\cii\models\Permission;
+use yii\base\Component;
+use yii\di\Instance;
+use yii\db\Connection;
+
 
 class DbManager extends Component {
+    public $db = 'db';
+
     public function init() {
         parent::init();
         $this->db = Instance::ensure($this->db, Connection::className());
@@ -13,7 +19,7 @@ class DbManager extends Component {
     
     public function checkAccess($userId, $packageId, $permissionId, $params = []) {
         $perm = Permission::find()
-            ->joinWith(['group as g', 'g.members as m'])
+            ->joinWith(['group as g', 'group.members as m'])
             ->where([
                 'permission_id' => $permissionId,
                 'package_id' => $packageId,
