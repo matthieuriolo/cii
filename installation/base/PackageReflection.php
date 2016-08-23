21e97f875a831db3da7d065bae054d8db5b68981
Matthieu Riolo <matthieu.riolo@googlemail.com>
2016-08-23 20:18:42 +0200

    correcting path in installation package reflection

--- installation/base/PackageReflection.php
+++ installation/base/PackageReflection.php
@@ -5,7 +5,16 @@ namespace app\base;
 use cii\base\PackageReflection as CPR;
 
 class PackageReflection extends CPR {
-	protected function getModulePath() {
+	protected function getInstallationPath() {
         return realpath(__DIR__ . '/../../modules');
+    }
+
+
+    protected function getInstalledVersion() {
+    	try {
+    		//catch errors silently since the cii tables might not exist
+    		return $parent::getInstalledVersion();
+    	}catch(\Exception $e) {}
+    	return null;
     }
 }
