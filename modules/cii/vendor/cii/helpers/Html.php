f7ec3a0c8cc79e35dd5dcac971056bb9faf17078
Matthieu Riolo <matthieu.riolo@googlemail.com>
2016-09-07 00:00:44 +0200

    adding visibility to backend menu

--- modules/cii/vendor/cii/helpers/Html.php
+++ modules/cii/vendor/cii/helpers/Html.php
@@ -32,42 +32,4 @@ class Html extends \yii\helpers\BaseHtml {
 
         return '<i class="glyphicon ' . $class . '"></i>';
     }
-
-
-    public static function printNestedMenu($prop, $selectedRouteCall = null, $lvl = 0) {
-        $label = '';
-        if(isset($prop['icon']) && !empty($prop['icon'])) {
-            $label .= '<i class="' . $prop['icon'] . '"></i>&nbsp;';
-        }
-
-        $label .= Html::encode($prop['name']);
-
-        if(isset($prop['url'])) {
-            $selected = false;
-            
-
-            if(is_callable($selectedRouteCall)) {
-                if($selectedRouteCall($prop)) {
-                    $selected = true;
-                }
-            }
-
-            echo Html::a($label, $prop['url'], [
-                'class' => 'list-group-item' . ($selected ? ' active' : ''),
-            ]);
-        }else {
-            echo Html::tag('span', $label, [
-                'class' => 'list-group-item',
-            ]);
-        }
-
-        if(isset($prop['children']) && !empty($prop['children'])) {
-            echo '<div class="list-group-item list-group-indented">';
-            foreach ($prop['children'] as $sub) {
-                static::printNestedMenu($sub, $selectedRouteCall, $lvl + 1);
-            }
-
-            echo '</div>';
-        }
-    }
 }
--- modules/cii/vendor/cii/widgets/BackendMenu.php
+++ modules/cii/vendor/cii/widgets/BackendMenu.php
@@ -21,7 +21,7 @@ class BackendMenu extends Widget {
 
         foreach(Yii::$app->cii->package->all(true) as $pkg) {
             $items = $pkg->getBackendItems();
-            Html::printNestedMenu($items, function($item) {
+            $this->printNestedMenu($items, function($item) {
                 $itemUrl = Url::toRoute($item['url']);
                 $appUrl = Yii::$app->getRequest()->getUrl();
                 
@@ -43,5 +43,51 @@ class BackendMenu extends Widget {
         }
 
         echo '</div>';
+    }
+
+
+    protected function printNestedMenu($prop, $selectedRouteCall = null, $lvl = 0) {
+        $label = '';
+        if(isset($prop['icon']) && !empty($prop['icon'])) {
+            $label .= '<i class="' . $prop['icon'] . '"></i>&nbsp;';
+        }
+
+        $label .= Html::encode($prop['name']);
+
+        
+
+        if(isset($prop['visible']) && !$prop['visible']) {
+            return false;
+        }
+        
+        if(isset($prop['url'])) {
+            $selected = false;
+            
+
+            if(is_callable($selectedRouteCall)) {
+                if($selectedRouteCall($prop)) {
+                    $selected = true;
+                }
+            }
+
+            echo Html::a($label, $prop['url'], [
+                'class' => 'list-group-item' . ($selected ? ' active' : ''),
+            ]);
+        }else {
+            echo Html::tag('span', $label, [
+                'class' => 'list-group-item',
+            ]);
+        }
+
+        if(isset($prop['children']) && !empty($prop['children'])) {
+            echo '<div class="list-group-item list-group-indented">';
+            foreach ($prop['children'] as $sub) {
+                $this->printNestedMenu($sub, $selectedRouteCall, $lvl + 1);
+            }
+
+            echo '</div>';
+        }
+
+        return true;
     }
 }
