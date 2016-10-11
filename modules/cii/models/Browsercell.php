73e80dc62cc377f23bb28583c5ee63bbf91cc309
Matthieu Riolo <matthieu.riolo@googlemail.com>
2016-10-11 21:24:16 +0200

    adding permission info to browser cell

--- modules/cii/models/Browsercell.php
+++ modules/cii/models/Browsercell.php
@@ -38,17 +38,27 @@ class Browsercell extends Model {
         return substr($this->file, strlen($this->basePath) + 1);
     }
 
+    public function isWritable() {
+        return is_writable($this->file);
+    }
+
     public function getDisplayContent($renameModalId) {
         return '<div class="element is-' . ($this->isDirectory() ? 'directory' : 'file') . '">' .
                 $this->getDisplayPreview() . 
                 '<div class="buttons">' .
-                    ($this->isDirectory() ? '<a class="button-open-directory" href="' . Url::to([$this->baseUrl . '/index', 'path' => $this->getFilePath()]) . '"><i class="glyphicon glyphicon-share"></i></a>' : '')
-                    . (!$this->isDirectory() ? '<a class="button-download" href="' . Url::to([$this->baseUrl . '/download', 'path' => $this->getFilePath()]) . '"><i class="glyphicon glyphicon-cloud-download"></i></a>' : '')
-                    . '<a class="button-rename" href="#" data-target="#' . $renameModalId . '" data-toggle="modal" data-original-name="'.Html::encode($this->getName()).'"><i class="glyphicon glyphicon-edit"></i></a>'
-                    . '<a class="button-remove" href="' . Url::to([$this->baseUrl . '/remove', 'path' => $this->getFilePath()]) . '"><i class="glyphicon glyphicon-remove"></i></a>'
-                    .
-
+                    ($this->isDirectory() ? '<a title="Open directory" class="button-open-directory" href="' . Url::to([$this->baseUrl . '/index', 'path' => $this->getFilePath()]) . '"><i class="glyphicon glyphicon-share"></i></a>' : '')
+                    . (!$this->isDirectory() ? '<a title="Download file" class="button-download" href="' . Url::to([$this->baseUrl . '/download', 'path' => $this->getFilePath()]) . '"><i class="glyphicon glyphicon-cloud-download"></i></a>' : '')
+                    . (
+                        $this->isWritable() ?
+                        '<a title="Rename element" class="button-rename" href="#" data-target="#' . $renameModalId . '" data-toggle="modal" data-original-name="'.Html::encode($this->getName()).'"><i class="glyphicon glyphicon-edit"></i></a>'
+                        . '<a title="Remove element" class="button-remove" href="' . Url::to([$this->baseUrl . '/remove', 'path' => $this->getFilePath()]) . '"><i class="glyphicon glyphicon-remove"></i></a>'
+                        :
+                        ''
+                    ) . 
                 '</div>' .
+                '<div class="permissions">' .
+                    ($this->isWritable() ? '' : '<i title="Not writable" class="glyphicon glyphicon-lock text-danger"></i>')
+                . '</div>' .
                 '<div class="name"><span>' . Html::encode($this->getName()) .'</span></div>' .
                 
             '</div>';
