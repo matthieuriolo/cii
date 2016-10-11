97813b083ba9299c02ca5759980b90866c83ae59
Matthieu Riolo <matthieu.riolo@googlemail.com>
2016-10-11 22:37:04 +0200

    adding allowChildren flag to route field

--- modules/cii/vendor/cii/fields/dropdown/RouteField.php
+++ modules/cii/vendor/cii/fields/dropdown/RouteField.php
@@ -8,11 +8,17 @@ class RouteField extends RelationField {
     public $baseUri = 'cii/route/view';
     public $label_property = 'slug';
 
+    public $allowChildren = true;
+
     public function findModel($pk) {
     	return Route::findOne($pk);
     }
 
 	public function getValues() {
+		if(!$this->allowChildren) {
+			return Yii::$app->cii->route->getParentRoutesForDropdown();
+		}
+
         return Yii::$app->cii->route->getRoutesForDropdown();
     }
 }
