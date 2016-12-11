028b5a5737bb91b0f8a235bd4424736785e89480
Matthieu Riolo <matthieu.riolo@googlemail.com>
2016-12-11 22:23:29 +0100

    removing subroute for backend; adding doc route for backend

--- modules/cii/routes/Backend.php
+++ modules/cii/routes/Backend.php
@@ -6,7 +6,23 @@ class Backend extends \cii\web\routes\ControllerRoute {
     public $baseRoute = 'cii/backend';
     public function getSubRoutes() {
     	return [
-    		'app\modules\cii\routes\BackendModules',
+    		[
+    			'class' => '\cii\web\routes\RegexRoute',
+    			'match' => 'modules\/(\w+)',
+    			'replace' => '$1/$*',
+    		],
+
+    		[
+    			'class' => '\cii\web\routes\RegexRoute',
+    			'match' => 'mandate\/(\w+)',
+    			'replace' => '$1/$*',
+    		],
+
+    		[
+    			'class' => '\cii\web\routes\RegexRoute',
+    			'match' => 'doc\/(\w+)',
+    			'replace' => '$1/$*',
+    		],
     	];
     }
 }
--- modules/cii/routes/BackendModules.php
+++
@@ -1,8 +0,0 @@
-<?php 
-
-namespace app\modules\cii\routes;
-
-class BackendModules extends \cii\web\routes\RegexRoute {
-    public $match = 'modules\/(\w+)';
-    public $replace = '$1/$*';
-}
