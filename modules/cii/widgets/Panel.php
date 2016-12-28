96349dbcc4fc659fe3a62d6b823624f9319562e4
Matthieu Riolo <matthieu.riolo@googlemail.com>
2016-12-28 20:33:04 +0100

    adding panel and tabbed panel widget

---
+++ modules/cii/widgets/Panel.php
@@ -0,0 +1,57 @@
+<?php 
+
+namespace app\modules\cii\widgets;
+
+use Yii;
+use yii\base\Widget;
+use cii\helpers\Html;
+
+class Panel extends Widget {
+    public $title;
+    public $encodeTitle = true;
+	public $content;
+    public $buttons = [];
+
+
+    protected function preRun($additionalHeaderContent = null) {
+        echo '<div class="panel panel-default tabbed-panel">';
+
+        echo '<div class="panel-heading">';
+        
+        if(is_array($this->buttons)) {
+            echo '<div class="pull-right btn-group btn-group-xs">';
+            foreach($this->buttons as $value) {
+                echo Html::a($value['label'], $value['url'], ['class' => 'btn btn-primary']);
+            }
+            
+            echo '</div>';
+        }
+
+        if($this->title) {
+            echo '<span class="title">';
+            
+            echo $this->encodeTitle ? Html::encode($this->title) : $this->title;
+
+            echo '</span>&nbsp;';
+        }
+
+        if($additionalHeaderContent) {
+            echo $additionalHeaderContent;
+        }
+
+        echo '</div>';
+            
+        echo '<div class="panel-body no-padding">';
+    }
+
+    protected function postRun() {
+        echo $this->content;
+        echo '</div>';
+        echo '</div>';
+    }
+
+    public function run() {
+        $this->preRun();
+        $this->postRun();
+    }
+}
---
+++ modules/cii/widgets/TabbedPanel.php
@@ -0,0 +1,31 @@
+<?php 
+
+namespace app\modules\cii\widgets;
+
+use Yii;
+use cii\helpers\Html;
+use yii\bootstrap\Tabs;
+
+class TabbedPanel extends Panel {
+    public $items = [];
+
+
+    public function run() {
+        $options = [];
+        foreach($this->items as $key => $value) {
+            $options[] = $value['label'];
+        }
+
+
+        $this->preRun(
+            Html::dropDownList($this->id . '-selector', null, $options, ['data-controller' => 'tabbed-panel-selector'])
+        );
+
+        echo Tabs::widget([
+            'id' => $this->id,
+            'items' => $this->items
+        ]);
+
+        $this->postRun();
+    }
+}
