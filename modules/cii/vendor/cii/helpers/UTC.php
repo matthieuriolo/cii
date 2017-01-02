ca2ab591c7d5b98176947181efad510442c75498
Matthieu Riolo <matthieu.riolo@googlemail.com>
2017-01-02 21:19:28 +0100

    adding a functions which returns all timezones (translated)

--- modules/cii/vendor/cii/helpers/UTC.php
+++ modules/cii/vendor/cii/helpers/UTC.php
@@ -1,6 +1,9 @@
 <?php
 namespace cii\helpers;
 
+use Yii;
+use yii\helpers\ArrayHelper;
+
 class UTC {
 	public static function datetime() {
         return gmdate('Y-m-d H:i:s');
@@ -14,4 +17,10 @@ class UTC {
 		$date = new \DateTime($str, new \DateTimeZone('UTC'));
 		return $date->getTimestamp();
 	}
+
+    public static function timezones() {
+        return ArrayHelper::map(timezone_identifiers_list(), 'id', function($val) {
+            return Yii::p('cii', $val);
+        });
+    }
 }
